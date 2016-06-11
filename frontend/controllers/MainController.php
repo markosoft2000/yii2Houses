bb25e482fd3ff238b7f2d0bdeca9f7616abc7a80
Marko <markosoft2000@yandex.ru>
2016-06-11 17:08:18 +0200

    lesson 1

---
+++ frontend/controllers/MainController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace frontend\controllers;
+
+
+use frontend\models\Image;
+
+class MainController extends \yii\web\Controller
+{
+    public function actionIndex()
+    {
+        $urlImage = Image::getImageUrl();
+        return $this->render('index', ['urlImage' => $urlImage]);
+    }
+
+}
